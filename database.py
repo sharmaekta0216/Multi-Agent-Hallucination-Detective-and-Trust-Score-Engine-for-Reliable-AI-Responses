409be3c97c198418d3366cf4a336340dc57705e3
Kavita <bhatikavita130@gmail.com>
2026-06-29 08:28:55 -0700

    upload datbase.py

--- database.py
+++ database.py
@@ -1,5 +1,12 @@
 import mysql.connector
 def connect_db():
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = " ",
+        database = " "
+    )
+    return connection
 def close_db():
 def execute_query():
 def fetch_data():
