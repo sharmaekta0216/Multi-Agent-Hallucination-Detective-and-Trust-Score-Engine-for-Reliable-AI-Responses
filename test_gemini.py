1eb8b4a516ac3e5a59b782f815dfcc851e829e4c
falakkumawat32 <falakkumawat32@gmail.com>
2026-07-01 16:22:14 +0530

    Backend

---
+++ test_gemini.py
@@ -0,0 +1,32 @@
+'''from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+response = model.generate_content("Hello! Can you tell me who am I?")
+
+print(response.text)'''
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+question = input("Enter your question: ")
+
+response = model.generate_content(question)
+
+print("\nGemini Response:")
+print(response.text)
